b94e88f38cb418dec7d7c83d14cc801b795af46c
Ismaila Bah <ismaila.bah@stu.ocsb.ca>
2023-12-18 12:25:31 -0500

    added code for cpt-01

---
+++ code.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+
+# Created by: Senai
+# Created on: December 2023
+# This program is the "Space Aliens" programs on the Badge
+
+
+import ugame
+import stage
+
+
+
+
+def game-scene():
+    # this function is the main game game_scene
+
+
+    print("\n\n\n") # 3 blank lines
+    print("Hello, Ismaila!")
+
+
+    # repeat forever, game loop
+    while True:
+        pass # just a placeholder for now
+
+
+if __name__ == "__main__":
+    game_scene()
