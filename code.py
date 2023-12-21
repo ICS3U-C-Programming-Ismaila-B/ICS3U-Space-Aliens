8c155feb873003a6824bef3f1d151b730abc6254
Ismaila Bah <ismaila.bah@stu.ocsb.ca>
2023-12-21 13:11:14 -0500

    created border

--- code.py
+++ code.py
@@ -14,7 +14,7 @@ def game_scene():
     # this function is the main game game_scene
 
     #image banks for cicuitpython
-    image_bank_background = stage.Bank.from_bmp16("space_aliens_backgound.bmp")
+    image_bank_background = stage.Bank.from_bmp16("space_aliens_background.bmp")
     image_bank_sprites = stage.Bank.from_bmp16("space_aliens.bmp")
     #set the background to image 0 in the image bank
     #  and the size (10x8 tiles of size 16x16)
@@ -22,11 +22,11 @@ def game_scene():
 
     background = stage.Grid(image_bank_background, 10, 8)
 
-    ship = stage.sprite(image_bank_sprites, 5, 75, constants.SCREEN_Y - (2 * constants.SPRITE_SIZE))
+    ship = stage.Sprite(image_bank_sprites, 5, 75, constants.SCREEN_Y - (2 * constants.SPRITE_SIZE))
 
     # create a stage for the background to show up on
     #  and set the frame rate to 60 fps
-    game = stage.stage(ugame.display,60)
+    game = stage.Stage(ugame.display,60)
     # set the layers of all sprites, items show up in order
     game.layers = [ship] + [background]
     # render all sprites
